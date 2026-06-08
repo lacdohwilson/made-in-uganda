d840ee57c27f636dada5e1069a961fa34d7bbf8a
lacdohwilsontchofor@gmail.com <user.email: lacdohwilsontchofor@gmail.com>
2026-06-08 23:56:04 +0100

    [feat] - Cloudinary image upload dimensions

---
+++ src/constants/cloudinary.constants.js
@@ -0,0 +1,12 @@
+export const CLOUDINARY_FOLDERS = {
+  PRODUCTS: 'made-in-cameroon/products',
+  USERS: 'made-in-cameroon/users',
+  STORES: 'made-in-cameroon/stores',
+};
+
+export const CLOUDINARY_DIMENSIONS = {
+  PRODUCT: { width: 640, height: 640, crop: 'fill', quality: 90 },
+  USER_PHOTO: { width: 200, height: 200, crop: 'fill', quality: 90 },
+  STORE_LOGO: { width: 300, height: 300, crop: 'fill', quality: 90 },
+  STORE_COVER: { width: 1200, height: 400, crop: 'fill', quality: 90 },
+};
